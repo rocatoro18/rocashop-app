e16729539f0feed22aa38f47a251ad28cfe09bbc
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2024-01-21 20:50:57 -0700

    Implementacion Login AuthDatasource

--- lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -1,6 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:teslo_shop/config/constants/environment.dart';
 import 'package:teslo_shop/features/auth/domain/domain.dart';
+import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
+
+// PATRON ADAPTADOR (NO IMPLEMENTADO) - clase que envulva dio y solo lo tengamos en un unico lugar
 
 class AuthDataSourceImpl extends AuthDataSource {
+  final dio = Dio(BaseOptions(baseUrl: Environment.apiUrl));
+
   @override
   Future<User> checkAuthStatus(String token) {
     // TODO: implement checkAuthStatus
@@ -8,9 +15,16 @@ class AuthDataSourceImpl extends AuthDataSource {
   }
 
   @override
-  Future<User> login(String email, String password) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<User> login(String email, String password) async {
+    try {
+      final response = await dio
+          .post('/auth/login', data: {'email': email, 'password': password});
+      // PASAR POR MAPPER
+      final user = UserMapper.userJsonToEntity(response.data);
+      return user;
+    } catch (e) {
+      throw WrongCredentials();
+    }
   }
 
   @override
---
+++ lib/features/auth/infrastructure/mappers/user_mapper.dart
@@ -0,0 +1,10 @@
+import 'package:teslo_shop/features/auth/domain/domain.dart';
+
+class UserMapper {
+  static User userJsonToEntity(Map<String, dynamic> json) => User(
+      id: json['id'],
+      username: json['fullName'],
+      email: json['email'],
+      roles: List<String>.from(json['roles'].map((rol) => rol)),
+      token: json['token']);
+}
