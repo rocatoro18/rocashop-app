a11639618926d3b6328dacb07b097491b1fe331e
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2024-01-25 12:54:28 -0700

    .

--- lib/features/auth/presentation/screens/check_auth_status_screen.dart
+++ lib/features/auth/presentation/screens/check_auth_status_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
+
 import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.dart';
 
 class CheckAuthStatusScreen extends ConsumerWidget {
