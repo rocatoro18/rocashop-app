5f7ebc26d0db5c2b37ecf43264dea267e257d231
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2024-01-25 13:50:44 -0700

    Init Router with routes protection

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,31 +1,34 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:teslo_shop/features/auth/auth.dart';
 import 'package:teslo_shop/features/products/products.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/splash',
-  routes: [
-    ///* Primera pantalla
-    GoRoute(
-        path: '/splash',
-        builder: (context, state) => const CheckAuthStatusScreen()),
+final goRouterProvider = Provider((ref) {
+  return GoRouter(
+      initialLocation: '/splash',
+      routes: [
+        ///* Primera pantalla
+        GoRoute(
+            path: '/splash',
+            builder: (context, state) => const CheckAuthStatusScreen()),
 
-    ///* Auth Routes
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: '/register',
-      builder: (context, state) => const RegisterScreen(),
-    ),
+        ///* Auth Routes
+        GoRoute(
+          path: '/login',
+          builder: (context, state) => const LoginScreen(),
+        ),
+        GoRoute(
+          path: '/register',
+          builder: (context, state) => const RegisterScreen(),
+        ),
 
-    ///* Product Routes
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const ProductsScreen(),
-    ),
-  ],
-
-  ///! TODO: Bloquear si no se está autenticado de alguna manera
-);
+        ///* Product Routes
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const ProductsScreen(),
+        ),
+      ],
+      redirect: (context, state) {
+        return null;
+      });
+});
