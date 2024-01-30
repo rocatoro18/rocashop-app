4e397a6fe6e1cc2c00a5f621716f49772b0a1852
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2024-01-29 19:21:22 -0700

    StateNotifierProvider - State

---
+++ lib/features/products/presentation/providers/products_provider.dart
@@ -0,0 +1,30 @@
+import 'package:teslo_shop/features/products/domain/domain.dart';
+
+// STATE
+// NOTIFIER
+// PROVIDER
+
+class ProductsState {
+  final bool isLastPage;
+  final int limit;
+  final int offset;
+  final bool isLoading;
+  final List<Product> products;
+
+  ProductsState({
+    this.isLastPage = false,
+    this.limit = 10,
+    this.offset = 0,
+    this.isLoading = false,
+    this.products = const [],
+  });
+
+  ProductsState copyWith(bool? isLastPage, int? limit, int? offset,
+          bool? isLoading, List<Product>? products) =>
+      ProductsState(
+          isLastPage: isLastPage ?? this.isLastPage,
+          limit: limit ?? this.limit,
+          offset: offset ?? this.offset,
+          isLoading: isLoading ?? this.isLoading,
+          products: products ?? this.products);
+}
